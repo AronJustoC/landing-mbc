fc6b73dd3e9ddc19a4b866108e594f94ed537966
Yonatan Pacco <yonatanpaccoylla@gmail.com>
2025-12-30 23:23:13 -0500

    Header - Hero - Contact - about

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,7 +11,9 @@ export default defineConfig({
   integrations: [
     icon({
       include: {
-        lucide: ['*'] // This tells astro-icon to include all icons from the 'lucide' set
+        lucide: ['*'], // This tells astro-icon to include all icons from the 'lucide' set
+        // tabler: ['brand-*'] // This tells astro-icon to include only the 'brand-whatsapp' icon from the 'tabler' set
+        'simple-icons': ['whatsapp', 'linkedin', 'youtube'] // This tells astro-icon to include only the 'whatsapp' icon from the 'simple-icons' set
       }
     })
   ]
