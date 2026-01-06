9cc5598863a357b1511ae4cc93c1063869655e2d
AronJustoC <aronchoque727@gmail.com>
2026-01-06 07:16:30 -0500

    remove @astrojs/image integration due to Vercel conflicts

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,7 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 import icon from "astro-icon";
 import tailwindcss from '@tailwindcss/vite';
-import image from "@astrojs/image";
+
 
 // https://astro.build/config
 export default defineConfig({
@@ -10,7 +9,6 @@ export default defineConfig({
     plugins: [tailwindcss()]
   },
   integrations: [
-    image(),
     icon({
       include: {
         lucide: ['*'], // This tells astro-icon to include all icons from the 'lucide' set
